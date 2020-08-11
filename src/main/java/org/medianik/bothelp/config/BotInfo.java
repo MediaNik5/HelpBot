1678dbc69c10206b337a9e558d6a1f5d693a5589
MediaNik <stafilopok@mail.ru>
2020-08-11 10:06:17 +0500

    .gitignore is now working

--- src/main/java/org/medianik/bothelp/config/BotInfo.java
+++
@@ -1,6 +0,0 @@
-package org.medianik.bothelp.config;
-
-public final class BotInfo {
-    public static String botName = "SchoolBot";
-    public static String token = "489873787:AAHzLVmcvAjy-ZuVvNxQ82qn-6KpdSUC6Uo";
-}
